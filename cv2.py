6234f5a5b0443b8535ad05706c34f40b461c1eb0
Frecautanu Ovidiu <70950196+Ovidiu2004@users.noreply.github.com>
2020-09-15 14:11:23 +0300

    Add files via upload

---
+++ cv2.py
@@ -0,0 +1,6 @@
+print("Frecautanu Ovidiu")
+print("04.06.2004.Moldova")
+print("ovidiu.frecautanu228@gmail.com")
+print("romina")
+print("necasatorit")
+print("Institutia Publica Liceul Teoretic Mihai Eminescu")
